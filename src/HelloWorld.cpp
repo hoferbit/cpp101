f1b4ca176ac5a6052e8eb1d194f5a54c940520f4
hoferbao <hoferbao@gmail.com>
2021-12-06 17:00:42 +0800

    g++

--- src/HelloWorld.cpp
+++ src/HelloWorld.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main(){
     cout << "Hello, world" << endl;
+    cout << "Size of char : " << sizeof(char) << endl;
+   cout << "Size of int : " << sizeof(int) << endl;
+   cout << "Size of short int : " << sizeof(short int) << endl;
+   cout << "Size of long int : " << sizeof(long int) << endl;
+   cout << "Size of float : " << sizeof(float) << endl;
+   cout << "Size of double : " << sizeof(double) << endl;
+   cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+   return 0;
     return 0;
 }
+
+// ➜  src git:(main) ✗ g++ HelloWorld.cpp
+// ➜  src git:(main) ✗ ./a.out  
